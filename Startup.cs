f0869922f8db056dd5780ca0edfcd18486a92227
Pedro Suarez <pfsuarez@gmail.com>
2020-10-28 17:13:00 -0300

    Added EF and dbcontext

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,10 @@ namespace Commander
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
             services.AddControllers();
+            
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
         }
 
