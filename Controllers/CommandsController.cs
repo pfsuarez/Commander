b814d2ae4c6c4a1373956fb5af8fe267c977335f
Pedro Suarez <pfsuarez@gmail.com>
2020-11-02 16:55:54 -0300

    Added Automapper and dtos

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,26 +13,35 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper mapper;
 
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             this._repository = repository;
+            this.mapper = mapper;
         }
 
+        // GET api/commands
         [HttpGet]
         public IActionResult GetAllCommands()
         {
             var commandsItems = _repository.GetAllCommands();
             
-            return Ok(commandsItems);
+            return Ok(mapper.Map<IEnumerable<CommandReadDto>>(commandsItems));
         }
 
+        // GET api/commands/{id}
         [HttpGet("{id}")]
         public IActionResult GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
 
-            return Ok(commandItem);
+            if(commandItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<CommandReadDto>(commandItem));
         }
     }
 }
