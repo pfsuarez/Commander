61cbbc04bd38a8681d776d56aea6957638952bc2
Pedro Suarez <pfsuarez@gmail.com>
2020-11-04 20:30:44 -0300

    Added ability to create commands

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -26,22 +26,35 @@ namespace Commander.Controllers
         public IActionResult GetAllCommands()
         {
             var commandsItems = _repository.GetAllCommands();
-            
+
             return Ok(mapper.Map<IEnumerable<CommandReadDto>>(commandsItems));
         }
 
         // GET api/commands/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public IActionResult GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
 
-            if(commandItem == null)
+            if (commandItem == null)
             {
                 return NotFound();
             }
 
             return Ok(mapper.Map<CommandReadDto>(commandItem));
+        }
+
+        //POST api/commands
+        [HttpPost]
+        public IActionResult CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
         }
     }
 }
